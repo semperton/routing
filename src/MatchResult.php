b7b814b8d1777bfcf38f4b0313b8400e18c5e93d
Johannes Rabausch <jrabausch@absatzformat.de>
2021-01-15 17:23:53 +0100

    Immutable MatchResult

--- src/MatchResult.php
+++ src/MatchResult.php
@@ -6,8 +6,39 @@ namespace Semperton\Routing;
 
 final class MatchResult
 {
-	public $isMatch = false;
-	public $handler = null;
-	public $methods = [];
-	public $params = [];
+	protected $match = false;
+
+	protected $handler = null;
+
+	protected $methods = [];
+
+	protected $params = [];
+
+	public function __construct(bool $match, $handler = null, array $methods = [], array $params = [])
+	{
+		$this->match = $match;
+		$this->handler = $handler;
+		$this->methods = $methods;
+		$this->params = $params;
+	}
+
+	public function isMatch(): bool
+	{
+		return $this->match;
+	}
+
+	public function getHandler()
+	{
+		return $this->handler;
+	}
+
+	public function getMethods(): array
+	{
+		return $this->methods;
+	}
+
+	public function getParams(): array
+	{
+		return $this->params;
+	}
 }
